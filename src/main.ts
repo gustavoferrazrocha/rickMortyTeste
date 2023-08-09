2a5d2264da71e912c8fb281b6cb49151d16f82cf
GustavoFerrazRocha <gustavoguusfr@gmail.com>
2023-08-09 15:09:14 -0300

    feat: installed pagination componente

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,25 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  }
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify);
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,18 @@ import axios from 'axios';
 import CardComponent from "../components/CardComponent.vue";
 
 let characters = ref<Array<Character>>([]);
+let currentPage = ref(1);
+
+let infoPagination = ref<InfoPagination>()
+
 const searchText = ref('');
+
+interface InfoPagination {
+  count: number,
+  pages: number,
+  next: string,
+  prev: string,
+}
 
 interface Character {
   id: number;
@@ -21,8 +32,18 @@ async function getAllCharacters () {
 
   try {
     const response = await axios.get('https://rickandmortyapi.com/api/character');
+    infoPagination.value = response.data.info;
+    characters.value = response.data.results;  
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function getNextPage () {
+  currentPage.value++;
+  try {
+    const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${currentPage.value}`);
     characters.value = response.data.results;
-    
   } catch (e) {
     console.error(e);
   }
@@ -48,7 +69,7 @@ onMounted(() => {
 
 
 <template>
-  <section className='min-h-screen flex flex-col items-center'>
+  <section className='min-h-screen flex flex-col items-center gap-4'>
     <input className='m-4 p-4 border-2' v-model="searchText" @input="searchCharacters" placeholder="Search characters">
     <main v-if="characters" className="grid 2xl:grid-cols-3 gap-5 xl:grid-cols-2 3xl:grid-cols-4">
       <CardComponent 
@@ -62,7 +83,13 @@ onMounted(() => {
       :image="character.image"
       />
     </main>
-
+    <v-pagination 
+    v-if="infoPagination" 
+    prev-icon="mdi-menu-left"
+    next-icon="mdi-menu-right"
+    :length="infoPagination.pages"
+    :total-visible="7">
+  </v-pagination>
   </section>
 </template>
 
