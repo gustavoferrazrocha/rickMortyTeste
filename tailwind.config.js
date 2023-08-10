67021a4b027738703aedc7868a47953c2ab4d83c
GustavoFerrazRocha <gustavoguusfr@gmail.com>
2023-08-10 14:01:54 -0300

    fix: eslint issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-import { tailwindcss, autoprefixer} from 'tailwindcss'
-export default {
+// Importando os módulos necessários usando require
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}",
@@ -9,25 +11,16 @@ export default {
     extend: {
       screens: {
         'sm': {'max': '640px'},
-        // => @media (min-width: 640px) { ... }
-  
         'md': '768px',
-        // => @media (min-width: 768px) { ... }
-  
         'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
         'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
         '2xl': '1800px',
-        // => @media (min-width: 1536px) { ... }
         '3xl': '2480px',
       }
     },
   },
   plugins: [
     tailwindcss,
-    autoprefixer
+    autoprefixer,
   ],
-}
+};
